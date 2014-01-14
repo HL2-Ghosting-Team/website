52b8b14f23812f5e53e135e5da3811bc329d4582
Michael Johnson <michael@nightexcessive.us>
2014-01-13 20:51:03 -0500

    Added test function for eq

---
+++ goapp/funcs_test.go
@@ -0,0 +1,25 @@
+package goapp
+
+import (
+	"testing"
+)
+
+func eqHelper(t *testing.T, expectedEq bool, args ...interface{}) {
+	if expectedEq {
+		if !eq(args...) {
+			t.Errorf("Expected %v to be equal, got false", args)
+		}
+	} else {
+		if eq(args...) {
+			t.Errorf("Expected %v to not be equal, got true", args)
+		}
+	}
+}
+
+func TestEq(t *testing.T) {
+	eqHelper(t, true, 1, 1)
+	eqHelper(t, true, 1, 1, 1)
+	eqHelper(t, false, 1, 1, 3)
+	eqHelper(t, false, 1, 2)
+	eqHelper(t, false, 1, 2, 3)
+}
